a19c5b58563b3560565b8a313cd253718e6327f4
Mavii1290 <110849665+Mavii1290@users.noreply.github.com>
2025-06-28 11:40:46 -0400

    back to showing

---
+++ src/pages/test.tsx
@@ -0,0 +1,18 @@
+// pages/test.tsx
+export default function TestPage() {
+  return (
+    <div
+      style={{
+        backgroundColor: "white",
+        color: "black",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "2rem",
+      }}
+    >
+      If you see this, the app works.
+    </div>
+  );
+}
