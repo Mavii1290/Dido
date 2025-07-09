8b55507310308ff529376057bbce7ddbccf0c3df
Mavii1290 <110849665+Mavii1290@users.noreply.github.com>
2025-07-09 18:46:34 -0400

    'fxed statc and image in ID'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,24 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+// import { useRouter } from "next/router";
+// import { useEffect } from "react";
 
-const Index: React.FC = () => {
-  const router = useRouter();
+// const Index: React.FC = () => {
+//   const router = useRouter();
 
-  useEffect(() => {
-    router.push("/dido");
-  }, [router]);
+//   useEffect(() => {
+//     router.push("/dido");
+//   }, [router]);
 
-  return (<div>
-        <div className="p-6">
-      <div className="bg-red-500 text-white p-4 md:bg-blue-500">
-        Tailwind Test: Red on mobile, Blue on desktop
-      </div>
-    </div>
-  </div>);
-};
+//   return (<div>
+//         <div className="p-6">
+//       <div className="bg-red-500 text-white p-4 md:bg-blue-500">
+//         Tailwind Test: Red on mobile, Blue on desktop
+//       </div>
+//     </div>
+//   </div>);
+// };
 
-export default Index;
+// export default Index;
+
+
+import Dido from "./dido";
+export default Dido;
