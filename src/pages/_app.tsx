a0ee9bc6bc98d21d64aa9b9c9c348fe0f1c882a5
Mavii1290 <110849665+Mavii1290@users.noreply.github.com>
2025-08-19 17:03:43 -0400

    'added servce worker'

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,28 @@
-
+// pages/_app.tsx
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../public/assets/scss/master.scss";
 import "../styles/extra.css";
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
+import { useEffect } from "react"; // Import useEffect
 
 function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Check if the browser supports Service Workers
+    if ("serviceWorker" in navigator) {
+      // Register the service worker file located at the root of your public directory
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((registration) => {
+          console.log("Service Worker registration successful:", registration);
+        })
+        .catch((error) => {
+          console.error("Service Worker registration failed:", error);
+        });
+    }
+  }, []); // The empty array ensures this effect runs only once on mount
+
   return <Component {...pageProps} />;
 }
 
